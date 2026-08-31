6c2363271cf808a1e4b2c447a1f106184fbc7d23
unknown <braitonhaule86@gmail.com>
2026-08-31 16:07:47 +0300

    yeah

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('blaze');
 });
+Route::get('/b', function () {
+    return view('b');
+});
+Route::get('/bray', function () {
+    return view('bray');
+});
